96cd67ed7a529d64e6432b0e998afd99e379903f
Manjinder Singh <msj003@gmail.com>
2015-03-20 15:39:07 +0530

    Login screen design

--- CarService/gen/netset/trainee/carservice/R.java
+++ CarService/gen/netset/trainee/carservice/R.java
@@ -8,6 +8,12 @@
 package netset.trainee.carservice;
 
 public final class R {
+    public static final class animator {
+        public static final int card_flip_left_in=0x7f040000;
+        public static final int card_flip_left_out=0x7f040001;
+        public static final int slide_down=0x7f040002;
+        public static final int zoom_in=0x7f040003;
+    }
     public static final class attr {
     }
     public static final class dimen {
@@ -17,8 +23,8 @@ public final class R {
          screen margins) for sw720dp devices (e.g. 10" tablets) in landscape here.
     
          */
-        public static final int activity_horizontal_margin=0x7f040000;
-        public static final int activity_vertical_margin=0x7f040001;
+        public static final int activity_horizontal_margin=0x7f050000;
+        public static final int activity_vertical_margin=0x7f050001;
     }
     public static final class drawable {
         public static final int bg=0x7f020000;
@@ -30,54 +36,63 @@ public final class R {
         public static final int usernamebg=0x7f020006;
     }
     public static final class id {
-        public static final int action_settings=0x7f080011;
-        public static final int edit_txt_client_address=0x7f08000d;
-        public static final int edit_txt_client_confrm_passwrd=0x7f08000f;
-        public static final int edit_txt_client_contact_no=0x7f08000c;
-        public static final int edit_txt_client_emaill=0x7f08000b;
-        public static final int edit_txt_client_name=0x7f08000a;
-        public static final int edit_txt_client_passwordd=0x7f08000e;
-        public static final int forgot_passwrd_btn=0x7f080006;
-        public static final int image_view_client=0x7f080007;
-        public static final int login_btn=0x7f080004;
-        public static final int logo=0x7f080000;
-        public static final int make_request=0x7f080009;
-        public static final int new_user_txt=0x7f080008;
-        public static final int paassword_edt_txt=0x7f080002;
-        public static final int remem_psswrd=0x7f080003;
-        public static final int sighn_up_submit_btn=0x7f080010;
-        public static final int signup_btn=0x7f080005;
-        public static final int username_edt_txt=0x7f080001;
+        public static final int action_settings=0x7f090016;
+        public static final int continueCustomerButton=0x7f090003;
+        public static final int continueServiceProviderButton=0x7f090004;
+        public static final int continueasTextView=0x7f090002;
+        public static final int edit_txt_client_address=0x7f090012;
+        public static final int edit_txt_client_confrm_passwrd=0x7f090014;
+        public static final int edit_txt_client_contact_no=0x7f090011;
+        public static final int edit_txt_client_emaill=0x7f090010;
+        public static final int edit_txt_client_name=0x7f09000f;
+        public static final int edit_txt_client_passwordd=0x7f090013;
+        public static final int forgot_passwrd_btn=0x7f09000b;
+        public static final int image_view_client=0x7f09000c;
+        public static final int loginFragment=0x7f090001;
+        public static final int loginScreenLayout=0x7f090005;
+        public static final int login_btn=0x7f090009;
+        public static final int logo=0x7f090000;
+        public static final int make_request=0x7f09000e;
+        public static final int new_user_txt=0x7f09000d;
+        public static final int paassword_edt_txt=0x7f090007;
+        public static final int remem_psswrd_checkbox=0x7f090008;
+        public static final int sighn_up_submit_btn=0x7f090015;
+        public static final int signup_btn=0x7f09000a;
+        public static final int username_edt_txt=0x7f090006;
     }
     public static final class layout {
         public static final int activity_main=0x7f030000;
-        public static final int login_screen=0x7f030001;
-        public static final int new_user_homepage=0x7f030002;
-        public static final int signup_screen=0x7f030003;
+        public static final int choose_login_fragment_layout=0x7f030001;
+        public static final int login_screen_fragment_layout=0x7f030002;
+        public static final int new_user_homepage=0x7f030003;
+        public static final int signup_screen=0x7f030004;
     }
     public static final class menu {
-        public static final int main=0x7f070000;
+        public static final int main=0x7f080000;
     }
     public static final class string {
-        public static final int Rem_passerd=0x7f050006;
-        public static final int action_settings=0x7f050001;
-        public static final int app_name=0x7f050000;
-        public static final int client_address_hint=0x7f05000d;
-        public static final int client_conf_passwrd_hint=0x7f05000f;
-        public static final int client_contact_no_hint=0x7f05000c;
-        public static final int client_email_hint=0x7f05000b;
-        public static final int client_name_hint=0x7f05000a;
-        public static final int client_passwrd_hint=0x7f05000e;
-        public static final int forgot_passwrd=0x7f050009;
-        public static final int hello_world=0x7f050002;
-        public static final int login_btn_txt=0x7f050007;
-        public static final int make_req_btn_txt=0x7f050012;
-        public static final int new_user_text=0x7f050011;
-        public static final int password=0x7f050004;
-        public static final int signup_btn_txt=0x7f050008;
-        public static final int submit_btn=0x7f050010;
-        public static final int username=0x7f050003;
-        public static final int username_edt_txt_hint=0x7f050005;
+        public static final int Rem_passerd=0x7f060006;
+        public static final int action_settings=0x7f060001;
+        public static final int app_name=0x7f060000;
+        public static final int client_address_hint=0x7f060010;
+        public static final int client_conf_passwrd_hint=0x7f060012;
+        public static final int client_contact_no_hint=0x7f06000f;
+        public static final int client_email_hint=0x7f06000e;
+        public static final int client_name_hint=0x7f06000d;
+        public static final int client_passwrd_hint=0x7f060011;
+        public static final int continueas=0x7f06000c;
+        public static final int customer=0x7f06000b;
+        public static final int forgot_passwrd=0x7f060009;
+        public static final int hello_world=0x7f060002;
+        public static final int login_btn_txt=0x7f060007;
+        public static final int make_req_btn_txt=0x7f060015;
+        public static final int new_user_text=0x7f060014;
+        public static final int password=0x7f060004;
+        public static final int serviceProvider=0x7f06000a;
+        public static final int signup_btn_txt=0x7f060008;
+        public static final int submit_btn=0x7f060013;
+        public static final int username=0x7f060003;
+        public static final int username_edt_txt_hint=0x7f060005;
     }
     public static final class style {
         /** 
@@ -101,10 +116,10 @@ public final class R {
     
  API 14 theme customizations can go here. 
          */
-        public static final int AppBaseTheme=0x7f060000;
+        public static final int AppBaseTheme=0x7f070000;
         /**  Application theme. 
  All customizations that are NOT specific to a particular API-level can go here. 
          */
-        public static final int AppTheme=0x7f060001;
+        public static final int AppTheme=0x7f070001;
     }
 }
--- CarService/src/netset/trainee/carservice/MainActivity.java
+++ CarService/src/netset/trainee/carservice/MainActivity.java
@@ -1,18 +1,48 @@
 package netset.trainee.carservice;
 
+import android.annotation.SuppressLint;
+import android.content.Context;
+import android.graphics.Point;
 import android.os.Bundle;
-import android.app.Activity;
+import android.support.v4.app.FragmentActivity;
+import android.view.Display;
 import android.view.Menu;
+import android.view.WindowManager;
+import android.widget.Button;
+import android.widget.FrameLayout;
 
-public class MainActivity extends Activity {
-
-    @Override
+public class MainActivity extends FragmentActivity {
+	
+	FrameLayout frameLayout;
+	Button continueCustomerButton,continuewServiceProviderButton;
+    FrameLayout.LayoutParams fragmentLayoutParams;
+	@SuppressLint("NewApi") @Override
     protected void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
+		super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        
+		WindowManager wm = (WindowManager) getSystemService(Context.WINDOW_SERVICE);
+		Display display = wm.getDefaultDisplay();
+		Point size = new Point();
+		display.getSize(size);
+		Constants.windowWidth= size.x;
+		Constants.windowHeight=size.y;
+   // 	fragmentLayoutParams=new FrameLayout.LayoutParams(Constants.windowWidth,(Constants.windowHeight)/2);
+		
+		initViews();
+        setListeners();
+       
+        
+        getSupportFragmentManager().beginTransaction().replace(R.id.loginFragment,new ChooseLoginFragment()).setCustomAnimations(R.animator.zoom_in,R.animator.slide_down, R.animator.zoom_in,R.animator.slide_down).addToBackStack(null).commit();
+    }
+    
+    public void initViews(){
+    	
     }
 
-
+    public void setListeners(){
+    	
+    }
     @Override
     public boolean onCreateOptionsMenu(Menu menu) {
         // Inflate the menu; this adds items to the action bar if it is present.
